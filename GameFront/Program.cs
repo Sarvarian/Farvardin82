85117596d5127aef2ff6629636fca0343a7e6da7
Sarvarian <Sarvarian@Hotmail.com>
2023-03-29 22:09:25 +0430

    add c#.net project as well

---
+++ GameFront/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new GameFront.Game1();
+game.Run();
---
+++ PlayerReplica/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PlayerReplica
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
